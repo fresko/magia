14c5625344f93650149bc2d1d8522114c500c0f3
Juan Paz <juan.paz.h@gmail.com>
2024-10-26 21:41:11 +0000

    prueba borde puntero

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const conteRoot = document.querySelector('.conte-root');
 const puntero = document.querySelector('.puntero');
+const ladoPuntero = 100;
 
+puntero.style.width = ladoPuntero + "px";
+puntero.style.height = ladoPuntero + "px";
+puntero.style.borderRadius = ladoPuntero + "px";
 
 conteRoot.addEventListener('mousemove', (e) => {
   console.log(e.pageX, e.pageY) 
-    puntero.style.left = (e.pageY - 50) + "px";
-    puntero.style.top = (e.pageX - 50) + "px";
+    puntero.style.top = (e.pageY - (ladoPuntero/2)) + "px";
+    puntero.style.left = (e.pageX - (ladoPuntero/2)) + "px";
 })
 
 
