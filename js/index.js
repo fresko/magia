bdb26786a59fb80998911fd76eaf99c1cc5d4081
Juan Paz <juan.paz.h@gmail.com>
2024-10-26 21:28:16 +0000

    prueba siguimiento a puntero

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-const conteRoot = document.getElementById('.conte-root');
+const conteRoot = document.querySelector('.conte-root');
+const puntero = document.querySelector('.puntero');
+
 
 conteRoot.addEventListener('mousemove', (e) => {
-  console.log(e.pageX, e.pageY)  });
+  console.log(e.pageX, e.pageY) 
+    puntero.style.left = (e.pageY - 50) + "px";
+    puntero.style.top = (e.pageX - 50) + "px";
+})
+
+
