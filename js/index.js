974fe2f006c5f4d4085163dc7489bc81eaae4832
Juan Paz <juan.paz.h@gmail.com>
2024-10-26 22:16:15 +0000

    prueba accion de  puntero  evento entra sale color

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
 const conteRoot = document.querySelector('.conte-root');
 const puntero = document.querySelector('.puntero');
+const btn = document.querySelector('.btn');
+
 const ladoPuntero = 100;
+const ladoPunteroGrande = 100;
+const btGrande = false;
+
+
 
 puntero.style.width = ladoPuntero + "px";
 puntero.style.height = ladoPuntero + "px";
 puntero.style.borderRadius = ladoPuntero + "px";
 
 conteRoot.addEventListener('mousemove', (e) => {
-  console.log(e.pageX, e.pageY) 
-    puntero.style.top = (e.pageY - (ladoPuntero/2)) + "px";
-    puntero.style.left = (e.pageX - (ladoPuntero/2)) + "px";
+   const lado = btGrande ? ladoPunteroGrande : ladoPuntero;
+    console.log(e.pageX, e.pageY) 
+    puntero.style.top = (e.pageY - (lado/2)) + "px";
+    puntero.style.left = (e.pageX - (lado/2)) + "px";
 })
 
+btn.addEventListener('mouseover', () => {
+  puntero.style.width = ladoPunteroGrande + "px";
+  puntero.style.height = ladoPunteroGrande + "px";
+  puntero.style.borderRadius = ladoPunteroGrande + "px";
+  btGrande = true;
+})
+
+
+btn.addEventListener('mouseout', () => {
+    puntero.style.width = ladoPunteroGrande + "px";
+    puntero.style.height = ladoPunteroGrande + "px";
+    puntero.style.borderRadius = ladoPunteroGrande + "px";
+    btn=false;
+})  
 
