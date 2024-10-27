c843ead1cb8e743e68c17f4e78956502f3e01caa
Juan Paz <juan.paz.h@gmail.com>
2024-10-27 03:11:07 +0000

    evento de triangulo animacion

--- js/index.js
+++ js/index.js
@@ -20,6 +20,9 @@ conteRoot.addEventListener('mousemove', (e) => {
     
     puntero.style.top = (e.pageY - (lado/2)) + "px";
     puntero.style.left = (e.pageX - (lado/2)) + "px";
+
+    createTriangle(e.pageX, e.pageY); 
+    createTriangle2(e.pageX-60, e.pageY-60); 
 })
 
 btn.addEventListener('mouseover', () => {
@@ -37,3 +40,47 @@ btn.addEventListener('mouseout', () => {
     btn=false;
 })  
 
+
+function createTriangle(x, y) {
+  const triangle = document.createElement('div');
+  triangle.className = 'triangle';
+  triangle.style.left = `${x}px`;
+  triangle.style.top = `${y}px`;
+
+  // Generar un color aleatorio para el triángulo (blanco, fondo del sitio, amarillo)
+  const colors = [
+      'rgba(255, 255, 255, 0.8)', // Blanco
+      'rgba(7, 22, 34, 0.8)',     // Fondo del sitio
+      'rgba(255, 255, 0, 0.8)'    // Amarillo
+  ];
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  triangle.style.borderBottomColor = color;
+
+  document.body.appendChild(triangle);
+
+  setTimeout(() => {
+      triangle.remove();
+  }, 2000); // Eliminar el triángulo después de 2 segundos
+}
+
+function createTriangle2(x, y) {
+  const triangle = document.createElement('puntero2');
+  triangle.className = 'triangle';
+  triangle.style.left = `${x}px`;
+  triangle.style.top = `${y}px`;
+
+  // Generar un color aleatorio para el triángulo (blanco, fondo del sitio, amarillo)
+  const colors = [
+      'rgba(255, 255, 255, 0.8)', // Blanco
+      'rgba(7, 22, 34, 0.8)',     // Fondo del sitio
+      'rgba(39, 245, 243, 0.8)'    // Amarillo
+  ];
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  triangle.style.borderBottomColor = color;
+
+  document.body.appendChild(triangle);
+
+  setTimeout(() => {
+      triangle.remove();
+  }, 2000); // Eliminar el triángulo después de 2 segundos
+}
